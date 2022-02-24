06b2170d8cc7aa2101fb72837d8ee3eda618ae68
tejas gk <tejasgk250@gmail.com>
2022-02-24 20:50:02 +0530

    some sorting algos

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using  namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)cin>>arr[i];
+    
+    for(int i=0;i<n-1;i++){
+        int min_index;
+        for(int j=i;j<n;j++){
+            if(arr[j]<arr[i]){
+                min_index=j;
+            }
+        }
+        swap(arr[i],arr[min_index]);
+
+    }
+    for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+
+}
