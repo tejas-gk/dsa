4979426ea8aeecc44844f1d0143753efc4ad9046
tejas gk <tejasgk250@gmail.com>
2022-02-25 19:45:55 +0530

    recursion and sorting

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -13,10 +13,20 @@ int permutation(int n){
     if(n==0)return 0;
     return n+permutation(n-1);
 }
+int power(int n){
+    if(n==0)return 1;
+    return n*power(n-1);
+}
+int factorial(int n){
+    if(n==0)return 1;
+    return n*factorial(n-1);
+}
 int main(){
     int n;
     cin>>n;
     for(int i=0;i<n;i++)cout<<fib(i)<<" ";cout<<endl;
-     for(int i=0;i<n;i++)cout<<addNum(i)<<" ";cout<<endl;
-        for(int i=0;i<n;i++)cout<<permutation(i)<<" ";cout<<endl;
+    for(int i=0;i<n;i++)cout<<addNum(i)<<" ";cout<<endl;
+    for(int i=0;i<n;i++)cout<<permutation(i)<<" ";cout<<endl;
+    for(int i=0;i<n;i++)cout<<factorial(i)<<" ";cout<<endl;
+        cout<<power(n)<<" ";cout<<endl;
 }
