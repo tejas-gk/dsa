8cefb2ea319a7f87b7c01de1bc7c44ca2c6c1b29
tejas gk <tejasgk250@gmail.com>
2022-02-25 19:44:36 +0530

    recursion and sorting

---
+++ recursion/recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n==0)return 0;
+    if(n==1)return 1;
+    return fib(n-1)+fib(n-2);
+}
+int addNum(int n){
+    if(n==0)return 0;
+    return n+addNum(n-1);
+}
+int permutation(int n){
+    if(n==0)return 0;
+    return n+permutation(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)cout<<fib(i)<<" ";cout<<endl;
+     for(int i=0;i<n;i++)cout<<addNum(i)<<" ";cout<<endl;
+        for(int i=0;i<n;i++)cout<<permutation(i)<<" ";cout<<endl;
+}
