71f3d7257d7a7940e1ce3f586a38236ce8eb9a3c
tejas._.gk <66897829+tejas-gk@users.noreply.github.com>
2022-03-05 18:25:09 +0530

    Update selection_sort.cpp

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -8,8 +8,8 @@ int main(){
     for (int i=0;i<n;i++)cin>>arr[i];
     
     for(int i=0;i<n-1;i++){
-        int min_index;
-        for(int j=i;j<n;j++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
             if(arr[j]<arr[i]){
                 min_index=j;
             }
